c9ae7ea80bc859b9cebc8dd5cef8882bfee341d3
alex8937 <chen@optym.local>
2017-01-20 11:44:40 -0500

    Finished Discuss03

--- dis/dis03/tree.py
+++ dis/dis03/tree.py
@@ -24,3 +24,43 @@ def max_tree(t):
         return root(t)
     else:
         return max(root(t), max([max_tree(b) for b in branches(t)]))
+
+def find_path(t, x):
+    def helper(t):
+        if root(t) == x:
+            return [x]
+        else:
+            path = [root(t)]
+            for b in branches(t):
+                if helper(b)[-1] == x:
+                    path.extend(helper(b))
+            return path
+    return helper(t) if helper(t)[-1] == x else None
+
+
+
+def prune(t, k):
+    if k == 0:
+        return tree(root(t))
+    else:
+        r = root(t)
+        bs = [prune(b, k - 1) for b in branches(t)]
+        return tree(r, bs)
+
+def main():
+    t = tree(1,
+            [tree(3,
+                [tree(4),
+                tree(5),
+                tree(6)]),
+            tree(12,
+                [tree(14),
+                tree(15),
+                tree(16)])])
+    print(find_path(t, 16))
+    print(find_path(t, 5))
+    print(find_path(t, 7))
+    print(prune(t, 1))
+
+
+if __name__ == "__main__": main()
