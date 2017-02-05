8b6504f803544fc87345dcf98947c7f37f97d381
alex8937 <chen@optym.local>
2017-02-05 11:36:21 -0500

    Finished lec18

--- lec/lec18/linked_list.py
+++ lec/lec18/linked_list.py
@@ -9,21 +9,15 @@ class Link:
         ## return str(self.first) + " -> " + repr(self.rest)
         """
         >>> Link(2, Link(3, Link(4)))
-        2->3->4
+        Link(2, Link(3, Link(4)))
         >>> Link(2)
-        2
+        Link(2)
         """
-        ##represent = str(self.first)
-        ##rest = self.rest
-        ##while rest:
-        ##    represent += "->" + str(rest.first)
-        ##    rest = rest.rest
-        ##return represent
         if self.rest is Link.empty:
-            rest = ""
+            rest_str = ""
         else:
-            rest = "->" + repr(self.rest)
-        return str(self.first) + rest
+            rest_str = ", " + repr(self.rest)
+        return "Link({0}{1})".format(self.first, rest_str)
 
     def __getitem__(self, index):
         """
@@ -55,3 +49,65 @@ class Link:
         while rest:
             length , rest = length + 1, rest.rest
         return length
+
+    @property
+    def second(self):
+        return self.rest.first
+
+    @second.setter
+    def second(self, value):
+        self.rest.first = value
+
+    def extend_link(self, t):
+        if self is Link.empty:
+            return t
+        else:
+            return Link(self.first, Link.extend_link(self.rest, t))
+
+    __add__ = extend_link
+    __radd__ = extend_link
+
+
+s = Link(2, Link(3, Link(4, Link(5))))
+square = lambda x : x * x
+odd = lambda x : x % 2 != 0
+
+def map_link(f, s):
+    if s is Link.empty:
+        return s
+    else:
+        return Link(f(s.first), map_link(f, s.rest))
+
+def filter_link(f, s):
+    if s is Link.empty:
+        return s
+    else:
+        filter_rest = filter_link(f, s.rest)
+        if f(s.first):
+            return Link(s.first, filter_rest)
+        else:
+            return filter_rest
+
+def join_link(s, delimiter):
+    if s is Link.empty:
+        return ""
+    elif s.rest is Link.empty:
+        return str(s.first)
+    else:
+        return str(s.first) + delimiter + join_link(s.rest, delimiter)
+
+def partition(n, m):
+    if n == 0:
+        return Link(Link.empty)
+    elif n < 0 or m == 0:
+        return Link.empty
+    else:
+        using_m = partition(n-m, m)
+        with_m = map_link(lambda p : Link(m, p), using_m)
+        without_m = partition(n, m-1)
+        return with_m + without_m
+
+def print_partition(n, m):
+    lists = partition(n, m)
+    lines = map_link(lambda s : join_link(s, ' + '), lists)
+    map_link(print, lines)
