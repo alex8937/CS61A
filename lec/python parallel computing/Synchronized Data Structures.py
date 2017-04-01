fcfe394cbfd1e619ef40fbd5a579c9205ea370a5
alex8937 <chen@optym.local>
2017-04-01 17:46:30 -0400

    Finished CS61A

---
+++ lec/python parallel computing/Synchronized Data Structures.py
@@ -0,0 +1,18 @@
+from queue import Queue
+import threading
+
+queue = Queue()
+
+def synchronized_consume():
+    while True:
+        print('got an item:', queue.get())
+        queue.task_done()
+def syschronized_produce():
+    consumer = threading.Thread(target=synchronized_consume, args=())
+    consumer.daemon = True
+    consumer.start()
+    for i in range(10):
+        queue.put(i)
+    queue.join()
+
+syschronized_produce()
